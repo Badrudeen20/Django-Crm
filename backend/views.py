dacc39b00c74230601c1bf1ff2af3eab96fdd83d
badrudeen20 <badruddinansary20@gmail.com>
2025-04-11 17:23:32 +0530

    permission-done

--- backend/views.py
+++ backend/views.py
@@ -74,20 +74,21 @@ def sidebarList(request,*args,**kwargs):
 @permission_required('Permission')
 def permission(request,*args,**kwargs):
    if 'Permission' in kwargs.get('module') and kwargs.get('access'):
-      if request.method == 'POST':
+      role = kwargs.get('role', '')
+      if request.method == 'POST' and request.headers.get('x-requested-with') == 'XMLHttpRequest' and 'View' in kwargs.get('permission'):
             start = request.POST['start']
             length = request.POST['length']
             search = request.POST['search']
             startIndex = (int(start)-1) * int(length)
             endIndex = startIndex + int(length)
             listData = []
+            roles = kwargs.get('roleIds')
             if search :
-                  data = Role.objects.using('default').filter(name__contains=search,id__in=[1,2])[startIndex:endIndex].all()
-                  totalLen = list(Role.objects.using('default').filter(name__contains=search,id__in=[1,2]).all())
+                  data = Role.objects.using('default').filter(name__contains=search,id__in=roles)[startIndex:endIndex].all()
+                  totalLen = list(Role.objects.using('default').filter(name__contains=search,id__in=roles).all())
             else:
-                  data = Role.objects.using('default').filter(id__in=[1,2])[startIndex:endIndex].all()
-                  totalLen = list(Role.objects.using('default').filter(id__in=[1,2]).all())
-            
+                  data = Role.objects.using('default').filter(id__in=roles)[startIndex:endIndex].all()
+                  totalLen = list(Role.objects.using('default').filter(id__in=roles).all())
 
             for i in data:
                   permission = {
@@ -103,11 +104,103 @@ def permission(request,*args,**kwargs):
                   "iTotalDisplayRecords":len(totalLen),
                   "aaData":listData
             }, status=200)
+      elif role:
+          if 'Edit' in kwargs.get('permission'):
+             module = Module.objects.using('default').all()
+             if request.method == 'POST':
+                
+                for m in module:
+                    perm = Permission.objects.using('default').filter(modules_id=m.id,role_id=role).first()
+                    if m.parent_id=='':
+                        if perm:
+                              if m.module in request.POST:
+                                perm.permission =  request.POST[m.module]
+                                perm.save()
+                              else:
+                                perm.delete()
+                        else:
+                              if m.module in request.POST:
+                                 Permission.objects.using('default').create(
+                                    permission=request.POST[m.module],
+                                    role_id = role,
+                                    modules_id = m.id
+                                 )
+                    else:
+                        if m.parent_id:
+                              permission = ''
+                              if m.module+'[view]' in request.POST:
+                                    permission += request.POST[m.module+'[view]']
+                              if m.module+'[add]' in request.POST:
+                                    permission +=','+request.POST[m.module+'[add]']
+                              if m.module+'[edit]' in request.POST:
+                                    permission +=','+request.POST[m.module+'[edit]']
+                              if m.module+'[delete]' in request.POST:
+                                    permission +=','+request.POST[m.module+'[delete]']
+                                       
+                              if perm:
+                                    if permission:
+                                       perm.permission =  permission
+                                       perm.save()
+                                    else:
+                                       perm.delete()  
+                              else:
+                                  if permission:
+                                     Permission.objects.using('default').create(
+                                          permission=permission,
+                                          role_id = role,
+                                          modules_id = m.id,
+                                          module_parent_id=m.parent_id
+                                     )
+                                   
+             allow = '<ul class="list-group">'
+             for m in module:
+                 perm = Permission.objects.using('default').filter(modules_id=m.id,role_id=role).first()
+                 if m.parent_id=='':
+                       allow +=(f'<li class="list-group-item ">'
+                                f'<div class="w-100 d-flex justify-content-between">'
+                                f'<div>{m.module}</div>'
+                                f'<div>View <input type="checkbox" name="{m.module}" value="View" {"checked" if perm else None} class="form-check-input" /> </div>'
+                                f'</div>'
+                                f'{checkParent(role,module,m.id)}'
+                                f'</li>')   
+                    
+             allow +='</ul>'
+             
+             return render(request,"backend/admin/allowpermission.html",{'permissions':allow})
+          else:
+            return redirect(reverse('backend:backend-permission'))
       else:
             return render(request,"backend/admin/permission.html")
    else:
       return render(request,"backend/404.html")  
-   
+
+def checkParent(role,module,mid):
+      allow = '<ul class="list-group">'
+      for m in module:
+            perm = Permission.objects.using('default').filter(modules_id=m.id,role_id=role).first()
+            if m.parent_id == str(mid):
+                        checked = []
+                        if perm:
+                           checked = perm.permission.split(',')
+                       
+                        allow +=(f'<li class="list-group-item">'
+                                    f'<div class="w-100 d-flex justify-content-between">'
+                                    f'<div>{m.module}</div>'
+                                    f'<div>View <input name="{m.module}[view]" type="checkbox" value="View" {"checked" if "View" in checked else None } class="form-check-input" />'
+                                    f'Add <input name="{m.module}[add]" type="checkbox" value="Add" {"checked" if "Add" in checked else None }  class="form-check-input" />' 
+                                    f'Edit <input name="{m.module}[edit]" type="checkbox" value="Edit" {"checked" if "Edit" in checked else None } class="form-check-input" />'
+                                    f'Delete <input name="{m.module}[delete]" type="checkbox" value="Delete" {"checked" if "Delete" in checked else None } class="form-check-input" /></div>'
+                                    f'</div>'
+                                    f'{checkParent(role,module,m.id)}'
+                                 f'</li>')   
+                 
+      allow +='</ul>'
+      
+      return allow
+
+
+
+
 @permission_required('Module')  
 def module(request,*args,**kwargs):
    if 'Module' in kwargs.get('module') and kwargs.get('access'):
@@ -151,6 +244,8 @@ def module(request,*args,**kwargs):
             return render(request,"backend/admin/module.html")
    else:
       return render(request,"backend/404.html")    
+
+
 
 @permission_required('Users')  
 def users(request,*args,**kwargs):
@@ -203,12 +298,23 @@ def users(request,*args,**kwargs):
 
 
 
-# def redirect(request):
-#     return render(request,"index.html")
-
 
 
 """
+def notfound(request):
+    return render(request,"backend/404.html")  
+
+def redirect(request):
+    return render(request,"index.html")
+
+allow = {}
+for m in module:
+      allow[m.module]= {}
+      perm = Permission.objects.using('default').filter(modules_id=m.id,role_id=role).first()
+      allow[m.module]['name'] = perm.permission if perm else None
+      allow[m.module]['module_id'] = perm.modules_id if perm else None
+      allow[m.module]['module_parent_id'] = perm.module_parent_id if perm else None
+
 def adminLogin(request):
    if request.method == 'POST':
          username = request.POST['username']
